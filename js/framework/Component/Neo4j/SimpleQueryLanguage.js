cce420a18eef99e9a6863220757a9b1b7c9d5e97
Zhihong SHEN <bluejoe2008@users.noreply.github.com>
2017-11-18 09:44:59 -0600

    supports :`label` on query filter
    supports :`label`
    this make giving non-english label possible

--- js/framework/Component/Neo4j/SimpleQueryLanguage.js
+++ js/framework/Component/Neo4j/SimpleQueryLanguage.js
@@ -12,11 +12,17 @@ define(['bundles/AppBundle/Component/Debug'],function (Debug) {
             var labels = "";
             var properties  = [];
             var regexp1 = new RegExp(/(:[a-zA-Z:]+)\s{0,}/);
+            var regexp12 = new RegExp(/(:`.*`)\s{0,}/);
             var regexp2 = new RegExp(/\s([a-z\]+)=([a-z0-9A-Z\s]+)\s?/);
 
             var matches1 = string.match(regexp1);
             if (matches1) {
                 labels = matches1[1];
+            }
+
+            var matches12 = string.match(regexp12);
+            if (matches12) {
+                labels = matches12[1];
             }
 
             var matches2 = string.match(regexp2);
